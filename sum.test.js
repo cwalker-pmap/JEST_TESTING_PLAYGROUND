ebe312d288e190afbcf40e5c89b9fabc9c9dbc41
Cyrus Walker <cwalker@processmap.com>
2016-09-22 11:34:48 -0400

    Adding Code Coverage tools from Jest

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,12 @@
 import sum from './sum'
+import subtract from './subtract'
 
 // using test global
 test('adds  1 + 2 to equal 3', () => {
   // using expect global
   expect(sum(1, 2)).toBe(3)
+})
+
+test('subtracts 2 - 1 to equal 1', () => {
+  expect(subtract(2,1)).toBe(1)
 })
